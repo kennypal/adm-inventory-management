f55bc00f0672afb05133bfc1bda0b2a1bbd36107
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-10-25 16:10:59 -0400

    css init

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import './App.css';
-import Home from './screens/Home'
-import AddMed from './screens/AddMed'
-import EditMed from './screens/EditMed'
-import Layout from './layouts/Layout'
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import Home from "./screens/Home";
+import AddMed from "./screens/AddMed";
+import EditMed from "./screens/EditMed";
+import Layout from "./layouts/Layout";
 
 function App() {
-
   return (
     <div className="App">
       <Layout>
-      <Route exact path='/'>
-        <Home />
-      </Route>
-      <Route path='/:id/add'>
-        <AddMed />
-      </Route>
-      <Route path='/:device_id/edit/:medication_id'>
-        <EditMed />
+        <Route exact path="/">
+          <Home />
         </Route>
-        </Layout>
+        <Route path="/:id/add">
+          <AddMed />
+        </Route>
+        <Route path="/:device_id/edit/:medication_id">
+          <EditMed />
+        </Route>
+      </Layout>
     </div>
   );
 }
--- client/src/components/DeleteMed.jsx
+++ client/src/components/DeleteMed.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
 import { destroyMedFromDevice } from '../services/medications'
 
 export default function DeleteMed(props) {
   const { deviceId, medicationId, meds, setMeds } = props
-  const history = useHistory()
 
   const deleteMed = async (deviceId, medicationId) => {
     await destroyMedFromDevice(deviceId, medicationId)
     setMeds(prevState => prevState.filter(med => {
       return med.id !== medicationId
     }))
-    // history.push('/')
   }
-
-  console.log(deviceId)
-  console.log(medicationId)
 
   return (
     <div>
